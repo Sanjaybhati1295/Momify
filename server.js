6a5d1de7b4e98324cb19bef75da5e0fbffadca50
Sanjaybhati1295 <44433514+Sanjaybhati1295@users.noreply.github.com>
2025-07-03 13:30:22 +0530

    server health added

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const WebSocket = require('ws');
 const http = require('http');
 require('dotenv').config();
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 'ok' }));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
 const wss = new WebSocket.Server({ server });
 
 
